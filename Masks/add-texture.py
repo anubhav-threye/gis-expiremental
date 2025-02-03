7b3d1e07c2e118b44fe9895443ed78972b2e5142
anubhav-threye <anubhav@threye.com>
2025-02-03 10:27:57 +0530

    Added a variable for matrial name prefix in mask add image texture script

---
+++ Masks/add-texture.py
@@ -0,0 +1,39 @@
+import bpy
+
+
+def remove_material_from_mesh(mesh):
+    # Check if the mesh has a material assigned
+    if mesh.data.materials:
+        # Remove the material from the mesh
+        mesh.data.materials.clear()
+
+
+def add_image_texture_material(mesh, path, mask_name):
+    # Add material to the mesh
+    material = bpy.data.materials.new(name=f"{material_name_prefix}_{mesh.name}")
+
+    # Set base color to image texture
+    material.use_nodes = True
+    bsdf = material.node_tree.nodes["Principled BSDF"]
+    texImage = material.node_tree.nodes.new("ShaderNodeTexImage")
+    texImage.image = bpy.data.images.load(f"{path}/{mask_name}_{mesh.name}.png")
+    material.node_tree.links.new(bsdf.inputs["Base Color"], texImage.outputs["Color"])
+
+    # Assign material to mesh
+    mesh.data.materials.append(material)
+
+
+# Constants
+path = r"E:\Projects\GIS\GIS-WSL\gis-expiremental\Masks\data\Bulit up\4k UDIM"
+mask_name = "bulitup"
+material_name_prefix = "BuiltUp"
+
+# Get all the meshes from the collection
+for mesh in bpy.context.collection.objects:
+    # Check if the object is a mesh
+    if mesh.type == "MESH":
+        # Remove the material from the mesh
+        remove_material_from_mesh(mesh)
+
+        # Add the material from the mesh
+        add_image_texture_material(mesh, path, mask_name)
